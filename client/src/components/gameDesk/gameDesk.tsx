70cda5ad2932efbae607498cdba0e9849ff5b018
Stanislau Ryzhkou <stanislaurv@gmail.com>
2024-03-18 18:22:51 +0100

    updated positioning of the cards

--- client/src/components/gameDesk/gameDesk.tsx
+++ client/src/components/gameDesk/gameDesk.tsx
@@ -1,4 +1,3 @@
-import { useCallback, useState } from "react";
 import { GameState } from "../../types";
 import PlayerBox from "./playerBoxes/playerBox";
 import EmptyPlayerBox from "./playerBoxes/emptyPlayerBox";
@@ -10,48 +9,64 @@ interface GameDeskProps {
 
 const GameDesk = ({ gameState, playerId }: GameDeskProps) => {
     const croupier = gameState?.isGameStarted ? gameState?.players[gameState?.players.length - 1] : null;
-    const players = gameState?.isGameStarted ? gameState?.players.slice(0, -1) : gameState?.players;
-    const emptySlots = Math.max(0, 4 - (players?.length || 0));
+    const players = gameState?.players && gameState.players.length > 1 ? gameState?.players.slice(0, -1) : []
+    const emptySlots = Math.max(0, 3 - (players?.length || 0));
+    const getPlayerPosition = (index: number, isEmpty = false) => {
+        const numberOfTheBox = isEmpty ? 
+            players?.length + index
+        : index;
+        if (numberOfTheBox === 0) {
+            return "absolute bottom-[10%] left-[50%] translate-x-[-50%]";
+        }
+        if (numberOfTheBox === 1) {
+            return "absolute bottom-[20%] left-[5%] translate-y-[-50%]";
+        }
+        if (numberOfTheBox === 2) {
+            return "absolute bottom-[20%] right-[5%] translate-y-[-50%]";
+        }
+        
+        return "";
+    };
 
     return (
-        <div className="h-screen  w-screen text-white py-8 overflow-y-auto">
-            <div className="mx-auto w-full px-4">
-                <div className="flex flex-col justify-between">
-                    {gameState?.isGameStarted && croupier && (
-                        <div className="mb-10 w-fit absolute top-[5%] left-[50%]">
-                            <PlayerBox
-                                key={croupier.id}
-                                {...croupier}
-                                isGameOver={gameState.isGameOver}
-                                isCurrentPlayer={croupier.id === playerId}
-                                results={gameState.results}
-                                isGameStarted={gameState.isGameStarted}
-                            />
-                        </div>
-                    )}
-                    <div className="flex justify-center gap-8">
-                        {players?.map((player) => (
-                            <PlayerBox
-                                key={player.id}
-                                {...player}
-                                isGameOver={gameState.isGameOver}
-                                results={gameState.results}
-                                isCurrentPlayer={player.id === playerId}
-                                isGameStarted={gameState.isGameStarted}
-                            />
-                        ))}
-
-                        {!gameState.isGameStarted &&
-                            Array.from({ length: emptySlots }).map((_, index) => (
-                                <EmptyPlayerBox key={index} />
-                            ))
-                        }
-                    </div>
+        <div className="h-screen w-screen text-white overflow-y-auto">
+            {gameState?.isGameStarted && croupier && (
+                <div className="absolute top-[5%] left-[50%] translate-x-[-50%] w-full">
+                    <PlayerBox
+                        key={croupier.id}
+                        {...croupier}
+                        isGameOver={gameState.isGameOver}
+                        isCurrentPlayer={croupier.id === playerId}
+                        results={gameState.results}
+                        isGameStarted={gameState.isGameStarted}
+                    />
                 </div>
+            )}
+            <div className="flex justify-center gap-8">
+                {players?.map((player, index) => (
+                    <div className={getPlayerPosition(index)} key={player.id}>
+                        <PlayerBox
+                            {...player}
+                            isGameOver={gameState.isGameOver}
+                            results={gameState.results}
+                            isCurrentPlayer={player.id === playerId}
+                            isGameStarted={gameState.isGameStarted}
+                        />
+                    </div>
+                ))}
+
+                {!gameState.isGameStarted &&
+                    Array.from({ length: emptySlots }).map((_, index) => (
+                        <div className={getPlayerPosition(index, true)} key={index}>
+                            <EmptyPlayerBox />
+                        </div>
+                    ))
+                }
             </div>
         </div>
     );
 };
+
 
 
 export default GameDesk;
